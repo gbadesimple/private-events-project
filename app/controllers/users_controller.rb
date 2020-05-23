f12785fdafcae580d25b9fd6be429fb3717537dd
gbadesimple <olugbadeolalekan@yahoo.com>
2020-05-23 15:57:51 +0100

    setup and sign-in complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
 
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
@@ -8,12 +12,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
+    @user.name.capitalize!
+    @user.email.downcase!
+    @user.username.downcase!
+
     if @user.save
 
       flash.notice = "Account Successfully created!"
-      redirect_to @user
+      redirect_to root_path
     else
       render :new
+    end
   end
 
 
@@ -21,6 +30,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
-
 
 end
