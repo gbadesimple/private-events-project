0e7663c6b2d270897dcb7087f9d0e29251f16a37
gbadesimple <olugbadeolalekan@yahoo.com>
2020-05-24 16:55:01 +0100

    Events controller, single event and list of all events created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations"}
   resources :users, only: [ :new, :create, :show ]
+  resources :events
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'users#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_152839) do
+ActiveRecord::Schema.define(version: 2020_05_23_162807) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_05_22_152839) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "username"
+    t.integer "created_events"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
