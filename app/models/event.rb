d4446d870f4d3a33c8f36231eaddd132e946c041
gbadesimple <olugbadeolalekan@yahoo.com>
2020-05-29 11:11:25 +0100

    model changes made

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ApplicationRecord
 
   belongs_to :creator, :class_name => "User"
 
-  has_many :attendances, :foreign_key => "attended_event_id"
+  has_many :attendances, :foreign_key => "attended_event_id", dependent: :destroy
   has_many :attendees, :through => "attendances", :source => "attendee"
 
 
