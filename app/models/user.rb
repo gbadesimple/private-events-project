208e8793c10bcff8f8a613a45489161d13323289
gbadesimple <olugbadeolalekan@yahoo.com>
2020-05-31 19:03:31 +0100

    bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  has_many :events, :foreign_key => "creator_id", :class_name => "Event"
+  has_many :events, :foreign_key => "creator_id", :class_name => "Event", dependent: :destroy
 
   has_many :attendances, :foreign_key => "attendee_id", dependent: :destroy
   has_many :attended_events, :through => "attendances"
