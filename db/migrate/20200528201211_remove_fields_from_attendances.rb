448f0b6c7956c6f4d604f944b7538fa787f7c0e2
gbadesimple <olugbadeolalekan@yahoo.com>
2020-06-04 09:10:53 +0100

    Columns attendances moved

--- db/migrate/20200528201211_remove_fields_from_attendances.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFieldsFromAttendances < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :attendances, :event_attended_id, :integer
-    remove_column :attendances, :event_attendee_id, :integer
-  end
-end
